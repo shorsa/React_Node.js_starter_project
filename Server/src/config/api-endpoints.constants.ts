957dfc9803d66f947573f65c1b8d252c6f66bdd3
Vlad Chernyshov <shorsavlad@gmail.com>
2021-02-24 11:26:04 +0200

    set up the project

---
+++ Server/src/config/api-endpoints.constants.ts
@@ -0,0 +1,12 @@
+export class ApiEndpointsConstants {
+    public static API: string = '/api';
+
+    public static EXAMPLE_FEATURE: string = '/example';
+    public static TEST: string = '/test';
+    
+    public static PROJECTS_FEATURE: string = '/example';
+    public static PROJECTS_CREATE: string = '/test';
+
+
+
+}
